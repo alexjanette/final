e35d30699744a5f5f19af60fbd3f636ad3e69e84
alexjanette <78947792+alexjanette@users.noreply.github.com>
2022-12-09 19:31:32 -0500

    Update Token.java

--- Token.java
+++ Token.java
@@ -1,25 +1,10 @@
 public class Token {
-
     int tokenCode;
     String lexeme;
-    private static final int UNKNOWN = 99
-    public Token(){
-        tokenCode = UNKNOWN;
-        lexeme = "";
-    }
-    public Token(int tokenCode, String lexeme){
+
+    public Token(String lexeme, int tokenCode,){
         this.tokenCode = tokenCode;
         this.lexeme = lexeme;
-    }
-    public void setTokenCode(int tokenCode){
-        this.tokenCode = tokenCode;
-    }
-    public int getTokenCode(){ return tokenCode; }
 
-    public void setLexeme(String lexeme){
-        this.lexeme = lexeme;
-    }
-
-    public String getLexeme(){ return lexeme; }
 
 }
